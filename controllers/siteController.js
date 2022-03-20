b3b530659a3c63ff153781dcd0634fc75fd05d4c
Sadia Sathi <sadia.nasrin10@northsouth.edu>
2022-03-21 02:00:03 +0600

    update code structure

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -1,13 +1,28 @@
 const landingPage = (req, res) => {
-    res.sendFile('Index.html', { root: './views' });
+    res.sendFile('index.html', { root: './views' });
 };
 
 const aboutPage = (req, res) => {
-    res.send('about page');
+    res.sendFile('about-us.html', { root: './views' });
+};
+
+const contactPage = (req, res) => {
+    res.sendFile('contact-us.html', { root: './views' });
+};
+
+const loginPage = (req, res) => {
+    res.sendFile('login.html', { root: './views' });
+};
+
+const signupPage = (req, res) => {
+    res.sendFile('sign-up.html', { root: './views' });
 };
 
 module.exports = {
     landingPage,
     aboutPage,
+    contactPage,
+    loginPage,
+    signupPage,
 }
 
--- routes/siteRoutes.js
+++ routes/siteRoutes.js
@@ -6,6 +6,12 @@ router.get('/', siteController.landingPage);
 
 router.get('/about', siteController.aboutPage);
 
+router.get('/contact', siteController.contactPage);
+
+router.get('/login', siteController.loginPage);
+
+router.get('/signup', siteController.signupPage);
+
 module.exports = {
     router
 };

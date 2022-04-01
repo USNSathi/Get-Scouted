b82c7a1425c6ba80133a15fdc6a8878dc8b29fd1
Sadia Sathi <sadia.nasrin10@northsouth.edu>
2022-04-02 04:51:32 +0600

    add routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const router = require('express').Router();
 
 const siteRoutes = require('./siteRoutes');
 const authRoutes = require('./authRoutes');
+const jobRoutes = require('./jobRoutes');
+const applicantRoutes = require('./applicantRoutes');
 
 router.use('/', siteRoutes);
 router.use('/auth', authRoutes);
+router.use('/jobs', jobRoutes);
+
+router.use('/applicants', applicantRoutes);
 
 module.exports = router;
